5cebc2c6ae2bb224d0e9a2b54948ece48d083daf
Sydney Pasloski <sydney.pasloski@ucalgary.ca>
2024-10-29 12:09:53 -0600

    moved data import to driver

---
+++ examples/driver.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+water_density = np.loadtxt("../data/water_density_vs_temp_usgs.txt", "float")
+air_density = np.loadtxt("../data/air_density_vs_temp_eng_toolbox.txt", "float")
+
+print(water_density)
+print(air_density)
+
+
+
+
+#if __name-- == "__main__":
