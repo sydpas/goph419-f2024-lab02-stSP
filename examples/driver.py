0baf152f473c894e08880b7e8ec4e1e5b4083a55
Sydney Pasloski <sydney.pasloski@ucalgary.ca>
2024-11-02 21:21:28 -0600

    added a space to my driver py to show my sister

--- examples/driver.py
+++ examples/driver.py
@@ -1,12 +1,19 @@
 import numpy as np
 
+from src.lab02.linalg_interp import (
+    gauss_iter_solve,
+)
+
 water_density = np.loadtxt("../data/water_density_vs_temp_usgs.txt", "float")
 air_density = np.loadtxt("../data/air_density_vs_temp_eng_toolbox.txt", "float")
 
 print(water_density)
 print(air_density)
 
+def main():
+    gauss_iter_solve(1, 2, 3,4,5)
 
 
 
-#if __name-- == "__main__":
+if __name__ == "__main__":
+    main()
