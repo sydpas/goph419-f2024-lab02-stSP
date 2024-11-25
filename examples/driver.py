44f27d004224c75d33a02ac0d4416e9a5c37d981
Sydney Pasloski <sydney.pasloski@ucalgary.ca>
2024-11-25 10:47:11 -0700

    modified driver.py

--- examples/driver.py
+++ examples/driver.py
@@ -11,21 +11,24 @@ air_density = np.loadtxt("../data/air_density_vs_temp_eng_toolbox.txt", "float")
 
 def main():
     # A = np.random.rand(3, 3)
-    A = np.array([[4, 0, 0],
-            [2, 3, 0],
-            [0, 4, 3]])
+    A = np.array([[4, -1, 0, 0],
+            [-1, 4, -1, 0],
+            [0, -1, 6, -1],
+            [0, 0, -1, 3]])
     # b = np.random.rand(3, 1)
-    b = np.array([1, 4, 3])
+    b = np.array([15, 10, 10, 10])
     # x0 = np.random.rand(3, 1)
-    x0 = np.array([1, 5, 6])
+    x0 = np.array([0, 0, 0, 0])
 
     print(f'A: {A}')
     print(f'b: {b}')
     print(f'x0: {x0}')
 
-    x = gauss_iter_solve(A, b, x0, 1e-8, None)
+    x_s = gauss_iter_solve(A, b, x0, 1e-8,'seidel')
+    print(f'X: {x_s}')
 
-    print(f'X: {x}')
+    x_j = gauss_iter_solve(A, b, x0, 1e-8,'jacobi')
+    print(f'X: {x_j}')
 
 
 if __name__ == "__main__":
