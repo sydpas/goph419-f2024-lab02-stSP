7b24ed3d181dda60d28bb4330a8f7ef843cf1a92
Sydney Pasloski <sydney.pasloski@ucalgary.ca>
2024-11-27 08:59:44 -0700

    updated spline function

--- src/lab02/linalg_interp.py
+++ src/lab02/linalg_interp.py
@@ -25,8 +25,15 @@ def gauss_iter_solve(A, b, x0, tol, alg):
     x: numpy.ndarray, shape of b.
     """
 
+    A = np.asarray(A, dtype=float) # converting A into an array if not already.
+    b = np.asarray(b, dtype=float) # converting b into an array if not already.
+    if x0 is None:
+        x0 = np.zeros_like(b, dtype=float) # initializing x0 if it is not given values.
+    else:
+        x0 = np.asarray(x0, dtype=float) # converting x0 into an array if not already.
+
     n = len(b)  # number of rows
-    max_iter = 1000  # maximum iterations to determine convergence
+    max_iter = 2000  # maximum iterations to determine convergence
 
     x = np.asarray(x0, dtype=float)  # converts x0 into an array if not already and stores in x
     if A.shape[0] != A.shape[1]:
@@ -36,15 +43,31 @@ def gauss_iter_solve(A, b, x0, tol, alg):
     if x.shape[0] != A.shape[0] and b.shape[0]:
         raise ValueError("x0 must have the rows as A and b.")
 
-    for iteration in range(max_iter):
-        x_new = x.copy()  # x_new holds a copy of x
-        for k in range(n):
-            a_row = A[k, :]  # each k, a_row will be a new row from A.
-            kp1 = (k + 1)
-            # isolate k. b[k] is from b at row k. subtract previous values, subtract remaining values, divide by diag.
-            x[k] = (b[k] - a_row[:k] @ x_new[:k] - a_row[kp1:] @ x[kp1:]) / A[k, k]
-        if np.linalg.norm(x_new - x) < tol:  # check for convergence of x_new compared to x
-            return x
+    if alg == 'seidel':
+        print(f'Algorithm used: Gauss-Seidel.')
+        for iteration in range(max_iter):
+            x_new = x.copy()  # x_new holds a copy of x
+            for k in range(n):
+                a_row = A[k, :]  # each k, a_row will be a new row from A.
+                kp1 = (k + 1)
+                # isolate k. b[k] is from b at row k. sub previous values, subtract remaining values, divide by diag.
+                x[k] = (b[k] - a_row[:k] @ x_new[:k] - a_row[kp1:] @ x[kp1:]) / A[k, k]
+            if np.linalg.norm(x_new - x) < tol:  # check for convergence of x_new compared to x
+                return x
+    elif alg == 'jacobi':
+        print(f'Algorithm used: Jacobi.')
+        for iteration in range(max_iter):
+            x_new = x.copy()
+            for k in range(n):
+                a_row = A[k, :]
+                kp1 = (k + 1)
+                # find x_new using the previous values
+                x_new[k] = (b[k] - np.dot(a_row[:k], x[:k]) - np.dot(a_row[kp1:], x[kp1:])) / A[k, k]
+            if np.linalg.norm(x_new - x) < tol:  # check for convergence of x_new compared to x
+                return x_new
+            x = x_new.copy()
+    elif alg != 'seidel' or 'jacobi':
+        raise ValueError("Please use either Gauss-Seidel or Jacobi algorithm.")
 
 
 def spline_function(xd, yd, order):
@@ -65,3 +88,18 @@ def spline_function(xd, yd, order):
     -----
     function: Takes one parameter and returns the interpolated y-value(s).
     """
+    n = len(yd)-1
+
+    # linear spline
+    if order == 1:
+        slope_list = np.zeros(n)
+        for i in range(len(yd) - 1):
+            ip1 = i + 1
+            slope_list[i] = (yd[ip1] - yd[i]) / (xd[ip1] - xd[i])  # equation 24
+        return slope_list
+
+    # quadratic spline
+    if order == 2:
+
+
+
