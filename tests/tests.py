f5f4327cdb41b7ec18f429f8027f6793a67b44d8
Sydney Pasloski <sydney.pasloski@ucalgary.ca>
2024-11-27 09:00:28 -0700

    idk what i did here

--- tests/tests.py
+++ tests/tests.py
@@ -1,2 +1,21 @@
-# if __name-- == "__main__":
+import numpy as np
 
+from src.lab02.linalg_interp import (
+    gauss_iter_solve,
+    spline_function
+)
+
+w_d = np.loadtxt("../data/water_density_vs_temp_usgs.txt", "float")
+a_d = np.loadtxt("../data/air_density_vs_temp_eng_toolbox.txt", "float")
+
+xd = np.array([1,2,3,4])
+yd = np.array([1,4,5,7])
+
+def main():
+
+    x = spline_function(xd, yd, 1)
+    print(f'Linear splines: {x}')
+
+
+if __name__ == "__main__":
+    main()
