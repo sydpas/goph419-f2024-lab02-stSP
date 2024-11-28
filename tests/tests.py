da8da54352b7676749a839dbff4d1bfb617c3b07
Sydney Pasloski <sydney.pasloski@ucalgary.ca>
2024-11-28 11:14:33 -0700

    added spline testing but not the one i need ugh

--- tests/tests.py
+++ tests/tests.py
@@ -8,13 +8,29 @@ from src.lab02.linalg_interp import (
 w_d = np.loadtxt("../data/water_density_vs_temp_usgs.txt", "float")
 a_d = np.loadtxt("../data/air_density_vs_temp_eng_toolbox.txt", "float")
 
-xd = np.array([1,2,3,4])
-yd = np.array([1,4,5,7])
+xd1, yd1 = w_d[:, 0], w_d[:, 1]
+xd2, yd2 = a_d[:, 0], a_d[:, 1]
 
 def main():
+    print(f'Using water density data...')
 
-    x = spline_function(xd, yd, 1)
-    print(f'Linear splines: {x}')
+    x_L_w = spline_function(xd1, yd1, 1)
+    x_L_w_rounded = [round(val, 4) for val in x_L_w]
+    print(f'Linear: {x_L_w_rounded}')
+
+    x_Q_w = spline_function(xd1, yd1, 2)
+    x_Q_w_rounded = [round(val, 4) for val in x_Q_w]
+    print(f'Quadratic: {x_Q_w_rounded}')
+
+    print(f'Using air density data...')
+
+    x_L_a = spline_function(xd2, yd2, 1)
+    x_L_a_rounded = [round(val, 4) for val in x_L_a]
+    print(f'Linear: {x_L_a_rounded}')
+
+    x_Q_a = spline_function(xd2, yd2, 2)
+    x_Q_a_rounded = [round(val, 4) for val in x_Q_a]
+    print(f'Quadratic: {x_Q_a_rounded}')
 
 
 if __name__ == "__main__":
