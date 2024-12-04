7d261a454ff0c953321f394e9bca8595077cad74
Sydney Pasloski <sydney.pasloski@ucalgary.ca>
2024-12-04 10:39:31 -0700

    fixed + finished graphs

--- examples/driver.py
+++ examples/driver.py
@@ -26,10 +26,6 @@ def main():
     x_cubic_water = np.linspace(min(xd1), max(xd1), 100)
     y_cubic_water = [cubic_spline(x) for x in x_cubic_water]
 
-    print(f'Linear: {y_linear_water[:5]}...')  # Check for non-zero results
-    print(f'Quadratic: {y_quad_water[:5]}...')
-    print(f'Cubic: {y_cubic_water[:5]}...')
-
     print(f'Using air density data...')
 
     linear_spline = spline_function(xd2, yd2, 1)
@@ -44,72 +40,67 @@ def main():
     x_cubic_air = np.linspace(min(xd2), max(xd2), 100)
     y_cubic_air = [cubic_spline(x) for x in x_cubic_air]
 
-    print(f'Linear: {y_linear_air[:5]}...')  # Check for non-zero results
-    print(f'Quadratic: {y_quad_air[:5]}...')
-    print(f'Cubic: {y_cubic_air[:5]}...')
-
     # now we will plot all the points
-
-    fig = plt.figure(figsize=(8, 6))
+    fig = plt.figure(figsize=(12, 8))  # width, height
 
     # linear plot for water
     plt.subplot(3, 2, 1)  # plot 1
-    plt.plot(x_linear_water, y_linear_water, label='Linear Spline', color='darkblue')
-    plt.title('Linear Spline', fontsize=14, fontweight='bold', color='darkblue')
-    plt.xlabel('X')
-    plt.ylabel('Y')
+    plt.plot(xd1, yd1, '-', color='darkblue', label='Data')
+    plt.plot(x_linear_water, y_linear_water, 'o', color='green', markersize=2, label='Linear Spline')
+    plt.xlabel('Temperature (°C)')
+    plt.ylabel('Water Density (g/cm\u00b3)')
     plt.legend()
     plt.grid(True)
 
     # quad plot for water
     plt.subplot(3, 2, 3)  # plot 2
-    plt.plot(x_linear_air, y_cubic_air, label='Cubic Spline', color='darkgreen')
-    plt.title('Quadratic Spline', fontsize=14, fontweight='bold', color='darkgreen')
-    plt.xlabel('X')
-    plt.ylabel('Y')
+    plt.plot(xd1, yd1, '--', color='darkgreen', label='Data')
+    plt.plot(x_quad_water, y_quad_water, 's', color='red', markersize=2, label='Quadratic Spline')
+    plt.xlabel('Temperature (°C)')
+    plt.ylabel('Water Density (g/cm\u00b3)')
     plt.legend()
     plt.grid(True)
 
     # cubic plot for water
     plt.subplot(3, 2, 5)  # plot 3
-    plt.plot(x_linear_water, y_cubic_water, label='Cubic Spline', color='darkred')
-    plt.title('Cubic Spline', fontsize=14, fontweight='bold', color='darkred')
-    plt.xlabel('X')
-    plt.ylabel('Y')
+    plt.plot(xd1, yd1, '-.', color='darkred', label='Data')
+    plt.plot(x_cubic_water, y_cubic_water, '^', color='blue', markersize=2, label='Cubic Spline')
+    plt.xlabel('Temperature (°C)')
+    plt.ylabel('Water Density (g/cm\u00b3)')
     plt.legend()
     plt.grid(True)
 
     # linear plot for air
     plt.subplot(3, 2, 2)  # 3 rows, 1 column, plot 1
-    plt.plot(x_linear_water, y_linear_water, label='Linear Spline', color='darkblue')
-    plt.title('Linear Spline', fontsize=14, fontweight='bold', color='darkblue')
-    plt.xlabel('X')
-    plt.ylabel('Y')
+    plt.plot(xd2, yd2, '-', color='cyan', label='Data')
+    plt.plot(x_linear_air, y_linear_air, 'x', color='purple', markersize=2, label='Linear Spline')
+    plt.xlabel('Temperature (°C)')
+    plt.ylabel('Air Density (kg/m\u00b3)')
     plt.legend()
     plt.grid(True)
 
     # quad plot for air
     plt.subplot(3, 2, 4)  # 3 rows, 1 column, plot 2
-    plt.plot(x_linear_air, y_cubic_air, label='Cubic Spline', color='darkgreen')
-    plt.title('Quadratic Spline', fontsize=14, fontweight='bold', color='darkgreen')
-    plt.xlabel('X')
-    plt.ylabel('Y')
+    plt.plot(xd2, yd2, '--', color='purple', label='Data')
+    plt.plot(x_quad_air, y_quad_air, 'p', color='hotpink', markersize=2, label='Quadratic Spline')
+    plt.xlabel('Temperature (°C)')
+    plt.ylabel('Air Density (kg/m\u00b3)')
     plt.legend()
     plt.grid(True)
 
     # cubic plot for air
     plt.subplot(3, 2, 6)  # 3 rows, 1 column, plot 3
-    plt.plot(x_linear_air, y_cubic_air, label='Cubic Spline', color='darkred')
-    plt.title('Cubic Spline', fontsize=14, fontweight='bold', color='darkred')
-    plt.xlabel('X')
-    plt.ylabel('Y')
+    plt.plot(xd2, yd2, '-.', color='hotpink', label='Data')
+    plt.plot(x_cubic_air, y_cubic_air, 'd', color='cyan', markersize=2, label='Cubic Spline')
+    plt.xlabel('Temperature (°C)')
+    plt.ylabel('Air Density (kg/m\u00b3)')
     plt.legend()
     plt.grid(True)
 
-    fig.text(0.28, 0.95, 'Water Density vs. Temperature', fontsize=12, fontweight='bold', ha='center')
+    fig.text(0.25, 0.95, 'Water Density vs. Temperature', fontsize=12, fontweight='bold', ha='center')
     fig.text(0.75, 0.95, 'Air Density vs. Temperature', fontsize=12, fontweight='bold', ha='center')
 
-    plt.tight_layout(rect=(0, 0, 0.92, 0.92))  # leave room for the title
+    plt.tight_layout(rect=(0, 0, 0.95, 0.95))  # leave room for the title
     plt.savefig('C:\\Users\\sydne\\git\\goph419\\goph419-f2024-lab02-stSP\\figures\\spline_functions.png')
     plt.show()
 
